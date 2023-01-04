a44e412d09ebc79f0145cbcd860828ae3620ff75
mariogomezb <mariogomezb@gmail.com>
2023-01-04 17:57:46 -0300

    subiendo proyecto

--- src/App.js
+++ src/App.js
@@ -1,69 +1,59 @@
 import './App.css';
+import { nanoid } from 'nanoid';
+import { baseColaboradores } from './api/baseColaboradores';
+import { useState } from 'react';
+
+
 
 function App() {
+  
+  const [colaboradores,setColaboradores]= useState(baseColaboradores)
+
+  const [nombre,setNombre]= useState('')
+  const [correo,setCorreo]= useState('')
+
+  const [buscar,setBuscar]=useState('')
+  
+  const handleSubmit =e=>{
+      e.preventDefault();
+      setColaboradores([...colaboradores,
+      {
+        nombre:nombre,
+        correo:correo,
+        id:nanoid(),
+      }])
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img
-          src="https://media3.giphy.com/media/13CoXDiaCcCoyk/giphy.gif?cid=ecf05e47mkcj61h3gjm3wjb2p4a067eiutl03vig47jsrksp&rid=giphy.gif&ct=g"
-          className="App-logo"
-          alt="cat shoulder shake"
-        />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn <span style={{ textDecoration: 'line-through' }}>Recat</span>{' '}
-          React
-        </a>
-      </header>
-      <main>
-        <p>
-          You probably meant to run <code>npx create-react-app</code> instead of{' '}
-          <code>npx create-recat-app</code>, but we're all here now and you
-          might as well enjoy some cats while you're at it.
-        </p>
-        <p className="images">
-          <img
-            src="https://media4.giphy.com/media/ICOgUNjpvO0PC/giphy.gif?cid=790b76118fbd3d2f0a35e8ff4036a66280840c2540f58474&rid=giphy.gif&ct=g"
-            alt="cat waving"
-          />
-          <img
-            src="https://media2.giphy.com/media/VbnUQpnihPSIgIXuZv/giphy.gif?cid=790b7611a46365babee79d6ce8d46d72eed5122eee0e6ef0&rid=giphy.gif&ct=g"
-            alt="cat developer"
-          />
-          <img
-            src="https://media3.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif?cid=790b7611e3ce0fc616ae6fb98d342b0b8131d965c75fac1b&rid=giphy.gif&ct=g"
-            alt="cat fast typing"
-          />
-          <img
-            src="https://media3.giphy.com/media/12PA1eI8FBqEBa/giphy.gif?cid=790b7611dfb7917b89f875b566ebedb7dc44135253c05172&rid=giphy.gif&ct=g"
-            alt="cat with chicks"
-          />
-          <img
-            src="https://media1.giphy.com/media/krZUvydC7Qrdu/giphy.gif?cid=790b7611c21df70db966c73cf4817e5bd79d5b4711ddc9f9&rid=giphy.gif&ct=g"
-            alt="cat riding ram"
-          />
-          <img
-            src="https://media3.giphy.com/media/10dU7AN7xsi1I4/giphy.gif?cid=790b7611a97153a9f993c7fc252257c0bb66399df74945ab&rid=giphy.gif&ct=g"
-            alt="cat all for one"
-          />
-          <img
-            src="https://media1.giphy.com/media/17Q92poP1qJwI/giphy.gif?cid=790b7611400844530bf6fe0debc6685cbcd4fff4ce6fd067&rid=giphy.gif&ct=g"
-            alt="cat teddy bear"
-          />
-        </p>
-        <p>
-          If you like tools that save you the pain of managing a build system,
-          check out Nx at{' '}
-          <a href="https://nx.dev/react">https://nx.dev/react</a>.
-        </p>
-      </main>
+    <div className="container bg-light pb-5 mt-5 border border-primary rounded">
+      <h1 className="text-center text-primary pt-3">Colaboradores</h1>
+
+      <div className="row">
+        <div className='col-6'>
+          <input className="mt-3 form-control" type="text" placeholder="Busqueda..." onChange={(e)=>setBuscar(e.target.value)}/>
+        </div>
+      </div>
+      <div className="row mt-3">
+        <div className="col-6">
+          <form onSubmit={handleSubmit}>
+            <input className="form-control" type="text" placeholder="Nombre" onChange={e=>setNombre(e.target.value)}/>
+            <input className="form-control mt-2" type="text" placeholder="E-mail" onChange={e=>setCorreo(e.target.value)}/>
+            <button className='btn btn-primary mt-2' type="submit">Agregar</button>
+          </form>
+        </div>
+      </div>
+      <div className="row ms-5">
+      <ul>
+        {colaboradores.filter((item)=>{
+          return item.nombre.toLowerCase().includes(buscar.toLowerCase());
+        })
+          .map((item)=>(
+            <li key={item.id}>{item.nombre} - {item.correo}</li>
+          ))
+        }
+      </ul>
+      </div>
     </div>
   );
 }
---
+++ src/api/baseColaboradores.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const baseColaboradores = [
+    {
+        id:"1",
+        nombre:"Colaborador 1",
+        correo:"colaborador1@colaborar1.com"
+    },
+    {
+        id:"2",
+        nombre:"Colaborador 2",
+        correo:"colaborador2@colaborar2.com"
+    },
+    {
+        id:"3",
+        nombre:"Colaborador 3",
+        correo:"colaborador3@colaborar3.com"
+    },
+]
